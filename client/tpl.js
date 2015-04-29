5a0f840e160155d024a2d9558d85fa43d611b6b5
Gwen <gwendall.esnault@gmail.com>
2015-04-29 18:45:51 +0200

    first

---
+++ client/tpl.js
@@ -0,0 +1,24 @@
+Template.items.animations({
+  ".item": {
+    container: ".container", // container of the ".item" elements
+    in: "fade-in", // class applied to inserted elements
+    out: "fade-out" // class applied to removed elements
+  }
+});
+
+Items = new Mongo.Collection("items", { connection: null });
+
+Template.items.helpers({
+  items: function() {
+    return Items.find();
+  }
+});
+
+Template.items.events({
+  "click [data-add-item]": function(e, tpl) {
+    Items.insert({ title: Fake.word() });
+  },
+  "click [data-remove-item]": function(e, tpl) {
+    Items.remove({ _id: this._id });
+  }
+});
