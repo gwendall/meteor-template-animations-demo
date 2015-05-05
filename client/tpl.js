64d88104dcc2768d658e4932c537ccaba0530d64
Gwen <gwendall.esnault@gmail.com>
2015-05-05 17:23:52 +0200

    update items

--- client/tpl.js
+++ client/tpl.js
@@ -1,11 +1,10 @@
-var getPic = function() {
-  return "http://accounts-cdn.9gag.com/media/default-avatar/1_" + _.random(0, 182) + "_100_v0.jpg";
+var insertRandomItem = function() {
+  Items.insert(_.extend(Fake.user(), { pic: "http://accounts-cdn.9gag.com/media/default-avatar/1_" + _.random(0, 182) + "_100_v0.jpg" }));
+
 }
 
 Items = new Mongo.Collection("items", { connection: null });
-_.each(_.range(0, 3), function(i) {
-  Items.insert(_.extend(Fake.user(), { pic: getPic() }));
-});
+_.each(_.range(0, 3), insertRandomItem);
 
 Template.items.helpers({
   items: function() {
@@ -15,7 +14,7 @@ Template.items.helpers({
 
 Template.items.events({
   "click [data-add-item]": function(e, tpl) {
-    Items.insert(_.extend(Fake.user(), { pic: getPic() }));
+    insertRandomItem();
   },
   "click [data-remove-item]": function(e, tpl) {
     Items.remove({ _id: this._id });
