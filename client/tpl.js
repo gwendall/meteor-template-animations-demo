30051f72f484da5b972adf83e20963258912544e
Gwen <gwendall.esnault@gmail.com>
2015-05-05 17:13:07 +0200

    update items

--- client/tpl.js
+++ client/tpl.js
@@ -1,17 +1,21 @@
+var getPic = function() {
+  return "http://accounts-cdn.9gag.com/media/default-avatar/1_" + _.random(0, 182) + "_100_v0.jpg";
+}
+
 Items = new Mongo.Collection("items", { connection: null });
 _.each(_.range(0, 3), function(i) {
-  Items.insert({ title: Fake.word() });
+  Items.insert(_.extend(Fake.user(), { pic: getPic() }));
 });
 
 Template.items.helpers({
   items: function() {
-    return Items.find();
+    return Items.find().fetch().reverse();
   }
 });
 
 Template.items.events({
   "click [data-add-item]": function(e, tpl) {
-    Items.insert({ title: Fake.word() });
+    Items.insert(_.extend(Fake.user(), { pic: getPic() }));
   },
   "click [data-remove-item]": function(e, tpl) {
     Items.remove({ _id: this._id });
