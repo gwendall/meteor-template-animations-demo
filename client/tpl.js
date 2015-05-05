0b780a7d4fa1f6eccb35b9cae395b2e37c87d86c
Gwen <gwendall.esnault@gmail.com>
2015-05-05 13:25:28 +0200

    demo callbacks

--- client/tpl.js
+++ client/tpl.js
@@ -1,13 +1,3 @@
-Template.items.animations({
-  ".item": {
-    animateInitial: true, // animate the intial elements
-    animateInitialStep: 200, // Step between each animation for each initial item
-    container: ".container-items", // container of the ".item" elements
-    in: "animated fast fadeInLeft", // class applied to inserted elements (animations courtesy of animate.css)
-    out: "animated fast fadeOutRight" // class applied to removed elements
-  }
-});
-
 Items = new Mongo.Collection("items", { connection: null });
 _.each(_.range(0, 3), function(i) {
   Items.insert({ title: Fake.word() });
@@ -25,5 +15,32 @@ Template.items.events({
   },
   "click [data-remove-item]": function(e, tpl) {
     Items.remove({ _id: this._id });
+  }
+});
+
+Logs = new Mongo.Collection("logs", { connection: null });
+Template.logs.helpers({
+  logs: function() {
+    return Logs.find().fetch().reverse();
+  }
+});
+
+
+Template.items.animations({
+  ".item": {
+    animateInitial: true, // animate the intial elements
+    animateInitialStep: 200, // Step between each animation for each initial item
+    animateInitialDelay: 0,
+    container: ".container-items", // container of the ".item" elements
+    in: "animated fast fadeInLeft", // class applied to inserted elements (animations courtesy of animate.css)
+    out: "animated fast fadeOutRight", // class applied to removed elements
+    inCallback: function() {
+      var title = $(this).find(".title").text();
+      Logs.insert({ text: "Inserted " + title + " to the DOM" });
+    },
+    outCallback: function() {
+      var title = $(this).find(".title").text();
+      Logs.insert({ text: "Removed " + title + " from the DOM" });
+    }
   }
 });
